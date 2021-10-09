6d36847f3dec7163f93eec5910fab9615f8cd1f7
nishanthdevan <91058956+nishanthdevan@users.noreply.github.com>
2021-10-09 13:05:10 +0530

    Add files via upload

---
+++ Guessrandomnumber.java
@@ -0,0 +1,32 @@
+package Randomnumber;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guessrandomnumber{
+
+	public static void main(String[]args) { 
+	     int answer,guess;
+	     final int MAX=100;
+	     Scanner sc = new Scanner(System.in);
+	     Random rand =new Random();
+	     boolean correct=false;
+	     answer= rand.nextInt(MAX)+1;
+	     while (!correct) {
+	     System.out.println("Guess a number between 1 and100:");
+	     guess =sc.nextInt();
+	  
+	     if (guess>answer) {
+	  	 System.out.println("Too high,try again");
+	         }
+	     else if (guess<answer) {
+	   	 System.out.println("Too low, try again");
+	    	 }
+	     else {          
+	     System.out.println("Yes, you guessed the number="+guess);
+	     correct=true;
+	   	     }
+	     }
+	     System.exit(0);
+	     sc.close();
+	}
+}
